765ef26d38dcfd64a69e34f33eaaf303453a5e5c
Saumen95 <saumenroy323@gmail.com>
2021-03-01 22:01:25 +0600

    urlpatterns

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,9 +1,8 @@
 
 from django.contrib import admin
-from django.urls import path
-from leads.views import home
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    path('leads/', include('leads.urls', namespace="leads"))
 ]
