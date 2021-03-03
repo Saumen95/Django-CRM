c7c6de27192da6d63ff8a44ed16020984785a7fe
Saumen95 <saumenroy323@gmail.com>
2021-03-03 21:37:28 +0600

    urls

--- leads/urls.py
+++ leads/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import lead_list, lead_details, lead_create
+from .views import lead_list, lead_details, lead_create,lead_update, lead_delete
 
 app_name = "leads"
 
 urlpatterns = [
-    path('', lead_list),
-    path('int:<pk>/', lead_details),
-    path("create/",lead_create),
+    path('', lead_list, name='lead-list'),
+    path('int:<pk>/', lead_details, name='lead-details'),
+    path("create/", lead_create,name='lead-create'),
+    path('int:<pk>/update/',name='lead-update'),
+    path('int:<pk>/delete/', lead_delete, name='lead-delete'),
 ]
